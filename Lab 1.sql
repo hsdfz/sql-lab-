13801ad79f7201c8a106db9b24c30f3f6ac08b02
anly560-lcheng <70456770+hsdfz@users.noreply.github.com>
2020-09-15 22:57:28 -0400

    Add files via upload

---
+++ Lab 1.sql
@@ -0,0 +1,6 @@
+
+select sakila.film.film_id, sakila.film.title, sakila.film_category.category_id, sakila.category.name from sakila.film 
+inner join sakila.film_category on sakila.film.film_id=sakila.film_category.film_id
+inner join sakila.category on sakila.film_category.category_id=sakila.category.category_id;
+
+
